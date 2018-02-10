d7edc73ea4ac32bdad2ffee8f31c3d1af9997e13
ka10ryu1 <lsyp.tuiovjo.1231@gmail.com>
2018-02-10 19:47:38 +0900

    comp->x, raw->y, img_rate->float

--- npz2jpg.py
+++ npz2jpg.py
@@ -4,9 +4,9 @@
 help = '作成したデータセット（.npz）の中身を画像として出力する'
 #
 
-#import os
+# import os
 import cv2
-#import sys
+# import sys
 import argparse
 import numpy as np
 
@@ -22,7 +22,7 @@ def command():
                         help='読み込む画像数（default: 10）')
     parser.add_argument('--random_seed', '-s', type=int, default=2,
                         help='乱数シード（default: 2, random: -1）')
-    parser.add_argument('--img_rate', '-r', type=int, default=4,
+    parser.add_argument('--img_rate', '-r', type=float, default=4,
                         help='画像サイズの倍率（default: 4）')
     parser.add_argument('-o', '--out_path', default='./result/',
                         help='・ (default: ./result/)')
@@ -32,23 +32,31 @@ def command():
 def main(args):
     # NPZ形式のファイルを読み込む
     np_arr = np.load(args.npz)
-    comp = np_arr['comp']
-    raw = np_arr['raw']
+    x = np_arr['x']
+    y = np_arr['y']
+
+    if(len(x.shape) > len(y.shape)):
+        y = [cv2.cvtColor(i, cv2.COLOR_GRAY2RGB) for i in y]
+        y = np.array(y)
+
+    if(len(x.shape) > len(y.shape)):
+        x = [cv2.cvtColor(i, cv2.COLOR_GRAY2RGB) for i in x]
+        x = np.array(x)
 
     # 全てを画像化するのは無駄なのでランダムに抽出する
     if(args.random_seed >= 0):
         np.random.seed(args.random_seed)
 
-    shuffle = np.random.permutation(range(len(comp)))
+    shuffle = np.random.permutation(range(len(x)))
     # ランダムに抽出した画像を結合する
-    # 上半分にはcompの画像をimg_numの数だけ
-    # 下半分にはrawの画像をimg_numの数だけ結合した画像を作成する
-    img = np.vstack((np.hstack(comp[shuffle[:args.img_num]]),
-                     np.hstack(raw[shuffle[:args.img_num]])))
+    # 上半分にはxの画像をimg_numの数だけ
+    # 下半分にはyの画像をimg_numの数だけ結合した画像を作成する
+    img = np.vstack((np.hstack(x[shuffle[:args.img_num]]),
+                     np.hstack(y[shuffle[:args.img_num]])))
     # そのままのサイズでは画像が小さいので、拡大する
-    w, h = img.shape[:2]
-    img = cv2.resize(img, (h * args.img_rate, w * args.img_rate),
-                     cv2.INTER_NEAREST)
+    size = (int(img.shape[1] * args.img_rate),
+            int(img.shape[0] * args.img_rate))
+    img = cv2.resize(img, size, cv2.INTER_NEAREST)
     # 作成した画像を表示・保存する
     cv2.imshow('test', img)
     cv2.waitKey(0)
